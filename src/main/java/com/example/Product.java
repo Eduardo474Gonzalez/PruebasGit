c994df82da6566fb896a0f9470f23d4719eeb4db
Eduardo González <119856502+Eduardo474Gonzalez@users.noreply.github.com>
2023-01-19 10:15:38 +0100

    Update Product.java

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,4 +1,15 @@
 package com.example;
 
 public class Product {
+  
+  private String category;
+  private boolean;
+  public Product(String category, boolean){
+    this.category = category;
+    this.boolean;
 }
+  public String getCategory() {
+    return category;
+  }
+  
+  
