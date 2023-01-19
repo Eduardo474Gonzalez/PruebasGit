a369633bb44eff41a2e2693fde1309e8320065a6
Eduardo González <119856502+Eduardo474Gonzalez@users.noreply.github.com>
2023-01-19 09:42:22 +0100

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hola desde Java")
+        System.out.println("Hola desde Java");
+            System.out.println("Fallo cambio");
     }
 }
