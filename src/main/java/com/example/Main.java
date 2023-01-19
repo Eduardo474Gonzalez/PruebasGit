5864cf3867674709958b7216a40d46d6f24219ff
Eduardo474Gonzalez <eduardogonzalezpavon@gmail.com>
2023-01-19 09:43:28 +0100

    Cambios conflicto

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("Pruebas");
         System.out.println("Pruebas desde IntelliJ IDEA");
         System.out.println("Hello World!");
-        System.out.println("Hola desde Java")
+        System.out.println("Hola desde Java");
+        System.out.println("Conflicto modificado");
+
     }
 }
