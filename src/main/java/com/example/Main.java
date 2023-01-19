3540490e1eea4d286bb2b333a99c7acd58133118
Eduardo474Gonzalez <eduardogonzalezpavon@gmail.com>
2023-01-19 09:32:00 +0100

    Cambios conflicto

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola desde Java");
+        System.out.println("Pruebas");
+        System.out.println("Pruebas desde IntelliJ IDEA");
+
     }
 }
